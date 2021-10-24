3a4ca098d8350be715eaf9e05bd645a282c860ee
Nayan <ndas5144@gmail.com>
2021-10-24 15:32:36 +0530

    Made update and delete button functionable

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 $insert = FALSE;
+$updated = FALSE;
+$deleted = FALSE;
 
 $servername = 'localhost';
 $username = 'root';
@@ -11,17 +13,49 @@ $conn = mysqli_connect($servername,$username,$password,$databse);
 if(!$conn){
   die("conn is false");
 }
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-  $title = $_POST['title'];
-  $description = $_POST['description'];
-
-$sql = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description')";
-
-$result = mysqli_query($conn,$sql);
-if($result){
-  $insert = TRUE;
+if (isset($_GET['delete'])){
+  $sno = $_GET['delete'];
+  $sql = "DELETE FROM `notes` WHERE `notes`.`sno` = $sno";
+  $result = mysqli_query($conn,$sql);
+  if($result){  
+    $deleted = TRUE;
+  }
+  else{
+    echo mysqli_error($conn);
+  } 
 }
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+  if (isset($_POST['snoEdit'])) {
+    $sno = $_POST['snoEdit'];
+    $title = $_POST['titleEdit'];
+    $description = $_POST['descriptionEdit'];
+
+    // $sql = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description')";
+    $sql = "UPDATE `notes` SET `title` = '$title', `description` = '$description' WHERE `notes`.`sno` = $sno ";
+
+    $result = mysqli_query($conn,$sql);
+    if($result){  
+      $updated = TRUE;
+    }
+    else{
+      echo mysqli_error($conn);
+    } 
+  }
+  else{
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    $sql = "INSERT INTO `notes` (`title`, `description`) VALUES ('$title', '$description')";
+
+    $result = mysqli_query($conn,$sql);
+    if($result){  
+      $insert = TRUE;
+    }
+    else{
+      echo mysqli_error($conn);
+    } 
+  }
 }
 
 ?>
@@ -38,49 +72,50 @@ if($result){
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
     integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
   <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
-  <title>PHP CRUD</title>
+  <title>PHP CRUD - Add your notes and save them</title>
 </head>
 
 <body>
   <!-- Button trigger modal -->
-<!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal">
+  <!-- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#editModal">
   Edit modal
 </button> -->
 
-<!-- Edit Modal -->
-<div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="editModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
+  <!-- Edit Modal -->
+  <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="editModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
         <!-- edit form -->
         <form action="/crud/index.php" method="POST">
-      <div class="form-group">
-        <label for="titleEdit">Note Title</label>
-        <input type="text" class="form-control" id="titleEdit" name="titleEdit" aria-describedby="emailHelp">
-      </div>
+          <div class="modal-body">
+            <input type="hidden" name="snoEdit" id="snoEdit">
+            <div class="form-group">
+              <label for="titleEdit">Note Title</label>
+              <input type="text" class="form-control" id="titleEdit" name="titleEdit" aria-describedby="emailHelp">
+            </div>
 
-      <div class="form-group">
-        <label for="descEdit">Note Description</label>
-        <textarea class="form-control" id="descriptionEdit" name="descriptionEdit" rows="3"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">Add Note</button>
-    </form>
+            <div class="form-group">
+              <label for="descEdit">Note Description</label>
+              <textarea class="form-control" id="descriptionEdit" name="descriptionEdit" rows="3"></textarea>
+            </div>
+          </div>
+          <div class="modal-footer d-block mr-auto">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Save changes</button>
+          </div>
+        </form>
         <!-- finish edit form inside modal -->
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
-</div>
-<!-- modal finish -->
+  <!-- modal finish -->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#">php CRUD</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -103,10 +138,32 @@ if($result){
       </ul>
     </div>
   </nav>
-<?php
+  <?php
 if($insert){
   echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+  <strong>Hurrey!</strong> Your note has been added successfully.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>';
+
+}
+?>
+  <?php
+if($updated){
+  echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Hurrey!</strong> Your note has been updated successfully.
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>';
+
+}
+?>
+  <?php
+if($deleted){
+  echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Hurrey!</strong> Your note has been deleted successfully.
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
@@ -115,7 +172,7 @@ if($insert){
 }
 ?>
   <div class="container my-4">
-    <h2>Add a note</h2>
+    <h2>Add your notes to PHP CRUD</h2>
     <form action="/crud/index.php" method="POST">
       <div class="form-group">
         <label for="title">Note Title</label>
@@ -136,12 +193,11 @@ if($insert){
           <th scope="col">S.No.</th>
           <th scope="col">Title</th>
           <th scope="col">Description</th>
-          <th scope="col">Time</th>
           <th scope="col">Actions</th>
         </tr>
       </thead>
       <tbody>
-      <?php
+        <?php
         $sql = "SELECT * FROM `notes` ";
         $result = mysqli_query($conn,$sql);
         $sno = 0;
@@ -151,44 +207,65 @@ if($insert){
             <th scope='row'>". $sno ."</th>
             <td>". $row['title']."</td>
             <td>". $row['description'] ."</td>
-            <td>". $row['timesmp'] ."</td>
-            <td><button type='submit' class='edit btn btn-primary'>Edit</button>  
-            <button type='submit'class='btn btn-primary'>Delete</button></td>
+            <td><button type='submit' class='edit btn btn-primary' id=".$row['sno'].">Edit</button> <button type='submit'class='delete btn btn-primary' id=d".$row['sno'].">Delete</button></td>
           </tr>";
         }
         ?>
-        </tbody>
+      </tbody>
     </table>
   </div>
 
   <!-- Optional JavaScript; choose one of the two! -->
 
   <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-  
-  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+    crossorigin="anonymous"></script>
   <script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
 
-    
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+    crossorigin="anonymous"></script>
   <script>
-    $(document).ready( function () {
+    $(document).ready(function () {
       $('#myTable').DataTable();
-    } );
+    });
   </script>
   <script>
     edits = document.getElementsByClassName('edit');
     Array.from(edits).forEach((element) => {
-      element.addEventListener("click",(e)=>{
-        console.log("edit ", );
+      element.addEventListener("click", (e) => {
+        console.log("edit ",);
         tr = e.target.parentNode.parentNode;
         title = tr.getElementsByTagName("td")[0].innerText;
         description = tr.getElementsByTagName("td")[1].innerText;
-        console.log(title,description);
+        console.log(title, description);
         titleEdit.value = title;
         descriptionEdit.value = description;
+        snoEdit.value = e.target.id;
+        console.log(e.target.id);
         $('#editModal').modal('toggle');
       })
-      
+
+    });
+    deletes = document.getElementsByClassName('delete');
+    Array.from(deletes).forEach((element) => {
+      element.addEventListener("click", (e) => {
+        console.log("delete ",);
+        sno = e.target.id.substr(1,);
+
+
+        if (confirm("Are you sure??")) {
+          console.log('yes');
+          window.location = `/crud/index.php?delete=${sno}`;
+        }
+        else {
+          console.log('no');
+        }
+      })
+
     });
   </script>
   <!-- Option 2: Separate Popper and Bootstrap JS -->
